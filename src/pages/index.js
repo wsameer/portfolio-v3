e1811a94cf59f08901f433e611b0772153fafcd9
nevrmore <dev.wsameer@gmail.com>
2020-06-20 22:30:11 +0530

    replaced header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
 import { Intro, Skills, Contact, Projects } from 'components/landing';
+import { Header } from 'components/theme';
 
 export default () => (
   <Layout>
     <SEO />
+    <Header />
     <Intro />
     <Projects />
     <Skills />
