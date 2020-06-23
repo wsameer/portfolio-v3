04603e36dd30a17386f5246112fa4f89c408fcbe
nevrmore <dev.wsameer@gmail.com>
2020-06-23 18:37:22 +0530

    navbar items changed

--- src/components/theme/Header/Navbar/index.jsx
+++ src/components/theme/Header/Navbar/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import logo from 'assets/icons/sameer-logo.png';
 import { Container } from 'components/common';
 import NavbarLinks from '../NavbarLinks';
 import { Wrapper } from './styles';
 
 const Navbar = () => (
   <Wrapper as={Container}>
-    <Link to="/">John Doe</Link>
+    <Link to="/">
+      <img src={logo} alt="" />
+    </Link>
     <NavbarLinks desktop />
   </Wrapper>
 );
