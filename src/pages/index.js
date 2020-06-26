32ba5722dc3dd755bce05edb1a3b25e241ac9dcd
nevrmore <dev.wsameer@gmail.com>
2020-06-26 22:25:58 +0530

    integrated new landing components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
 import { Intro, About, Contact, Work, Experience } from 'components/landing';
-import { Header } from 'components/theme';
 
 export default () => (
   <Layout>
     <SEO />
-    <Header />
     <Intro />
     <About />
     <Experience />
