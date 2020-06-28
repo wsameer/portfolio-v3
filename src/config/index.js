47f8937cccda72a86568b1c9d69dc756b8c2f7bb
nevrmore <dev.wsameer@gmail.com>
2020-06-28 15:30:38 +0530

    relocated the config file

---
+++ src/config/index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  defaultTitle: 'Sameer Waskar',
+  logo: 'https://wsameer.github.io/v1/img/sameer-logo.svg',
+  author: 'Sameer Waskar',
+  url: 'https://wsameer.github.io/v1/',
+  legalName: 'Sameer Waskar',
+  defaultDescription: 'I’m Sameer and I’m a Full stack engineer!',
+  socialLinks: {
+    github: 'https://github.com/wsameer',
+    linkedin: 'https://www.linkedin.com/in/wsameer',
+    instagram: 'https://instagram.com/the_savage_lens',
+    youtube: 'https://www.youtube.com/user/wsameer',
+    google: 'https://plus.google.com/u/0/wsameer',
+  },
+  // googleAnalyticsID: 'UA-88875900-4',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  address: {
+    city: 'Pune',
+    state: 'Maharashtra',
+    country: 'India',
+    zipCode: '411030',
+  },
+  contact: {
+    email: 'dev.wsameer@gmail.com',
+    phone: 'phone number',
+  },
+  foundingDate: '2020',
+};
