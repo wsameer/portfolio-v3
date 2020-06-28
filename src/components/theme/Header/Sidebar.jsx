e7cffc429f516b5e12de15ae0bfd737574a8d860
nevrmore <dev.wsameer@gmail.com>
2020-06-28 15:29:37 +0530

    removing sidebar component

--- src/components/theme/Header/Sidebar.jsx
+++ src/components/theme/Header/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NavbarLinks from './NavbarLinks';
 import { SidebarWrapper } from './styles';
 
-const Sidebar = ({ sidebar, toggle }) => (
+const Sidebar = ({ sidebar }) => (
   <SidebarWrapper active={sidebar}>
     <NavbarLinks />
   </SidebarWrapper>
