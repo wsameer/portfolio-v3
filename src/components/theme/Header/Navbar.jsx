1a580693ea329e9ecf55d1542ed739410d8a56f6
nevrmore <dev.wsameer@gmail.com>
2020-06-30 21:25:49 +0530

    logo repositioning

--- src/components/theme/Header/Navbar.jsx
+++ src/components/theme/Header/Navbar.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import logo from 'assets/icons/sameer-logo.png';
 import { Container } from 'components/common';
+import styled from 'styled-components';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 import Hamburger from './Hamburger';
 import NavbarLinks from './NavbarLinks';
-import { NavbarWrapper } from './styles';
 
-const Navbar = ({ sidebar, toggle }) => (
-  <NavbarWrapper as={Container}>
-    <Link to="/">
+const NavbarWrapper = styled.div`
+  padding: 0.6rem 0;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #2d3031;
+  z-index: 4;
+
+  img {
+    position: static;
+    margin-left: 0.5rem;
+    height: 32px;
+    margin-bottom: 0;
+  }
+
+  /* desktop */
+  @media only screen and (min-width: 992px) {
+    width: 100% !important;
+    img {
+      margin-left: 1.5rem;
+    }
+  }
+`;
+
+const Navbar = ({ sidebar, toggleSidebar }) => (
+  <NavbarWrapper sidebar as={Container}>
+    <AnchorLink offset="50" href="#intro">
       <img src={logo} alt="" />
-    </Link>
+    </AnchorLink>
     <NavbarLinks desktop />
-    <Hamburger sidebar={sidebar} toggle={toggle} />
+    <Hamburger sidebar={sidebar} toggleSidebar={toggleSidebar} />
   </NavbarWrapper>
 );
 
