653461dbf40988c1fb97a292a530abe4ab78c72e
nevrmore <dev.wsameer@gmail.com>
2020-07-04 10:28:21 +0530

    import changes

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,6 +1,1 @@
-export * from './Layout';
-export * from './Container';
-export * from './Button';
-export * from './Card';
-export * from './Input';
-export * from './SEO';
+export * from '../Layout';
