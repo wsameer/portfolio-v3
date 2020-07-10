4fef756b0ddc418aff57f10475472da62b345cf8
nevrmore <dev.wsameer@gmail.com>
2020-07-10 16:08:17 +0530

    file deleted

--- src/components/landing/index.js
+++
@@ -1,7 +0,0 @@
-import { Experience } from './Experience';
-
-export { Intro } from './Intro';
-export { About } from './About';
-export { Contact } from './Contact';
-export { Work } from './Work';
-export { Experience } from './Experience';
