874d430f89a335f499ce571761e28378619832e8
nevrmore <dev.wsameer@gmail.com>
2020-07-16 18:49:28 +0530

    added icons for education and experience

--- src/components/TimelineContent.jsx
+++ src/components/TimelineContent.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { Button } from 'styles';
+import { Accordion } from './Accordion';
 
 const StyledContent = styled.div`
   position: relative;
@@ -11,7 +13,7 @@ const StyledTime = styled.span`
 `;
 
 const StyledEmployer = styled.h3`
-  color: #f4eeeb;
+  color: #fe5029;
   font-size: 20px;
   margin-top: 10px;
   font-weight: 500;
@@ -20,15 +22,38 @@ const StyledEmployer = styled.h3`
 
 const StyledJobTitle = styled.p`
   font-weight: 400;
-  margin-bottom: 10px;
+  margin-bottom: 30px;
   font-size: 18px;
-  color: #f4eeeb;
+  color: #fe5029;
 `;
 
-export const TimelineContent = ({ data }) => (
-  <StyledContent>
-    <StyledTime>{data.timeline}</StyledTime>
-    <StyledEmployer>{data.title}</StyledEmployer>
-    <StyledJobTitle>{data.subTitle}</StyledJobTitle>
-  </StyledContent>
-);
+const StyledReadMore = styled.a`
+  font-size: 12px;
+
+  &:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`;
+
+export const TimelineContent = ({ data }) => {
+  const [open, setOpen] = useState(false);
+
+  const toggleAccordion = () => setOpen(!open);
+
+  return (
+    <StyledContent>
+      <StyledTime>{data.timeline}</StyledTime>
+      <StyledEmployer>{data.title}</StyledEmployer>
+      <StyledJobTitle>{data.subTitle}</StyledJobTitle>
+      {data.responsibilies && (
+        <>
+          <StyledReadMore onClick={toggleAccordion}>{open ? 'Hide More' : 'Read More'}</StyledReadMore>
+          <Accordion points={data.responsibilies} open={open} />
+        </>
+      )}
+
+      {data.major && <p>{data.major}</p>}
+    </StyledContent>
+  );
+};
