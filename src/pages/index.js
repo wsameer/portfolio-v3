fe50ca31b4ba4827e68d53d9983cdc6c85afb535
nevrmore <dev.wsameer@gmail.com>
2020-07-19 17:22:54 +0530

    reverted the code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,44 @@
+/* eslint-disable react/no-danger */
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Hero, About, Contact, Work, Experience, SEO } from 'components';
 import { Layout } from 'components/Layout';
 import { graphql } from 'gatsby';
 
-export const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO />
-    <Hero data={data.hero.edges} />
-    <About />
-    <Experience />
-    <Work />
-    <Contact />
-  </Layout>
-);
+const IndexPage = ({ data }) => {
+  const { markdownRemark } = data; // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark;
 
-// GraphQL query
+  return (
+    // <Layout>
+    //   <SEO />
+    //   <Hero />
+    //   <About />
+    //   <Experience />
+    //   <Work />
+    //   <Contact />
+    // </Layout>
+    <Layout>
+      <div className="blog-post-container">
+        <div className="blog-post">
+          <h1>{frontmatter.title}</h1>
+          <h2>{frontmatter.date}</h2>
+          <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default IndexPage;
+
 export const pageQuery = graphql`
-  {
-    hero: allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/hero/" } }) {
-      edges {
-        node {
-          frontmatter {
-            title
-            name
-            subtitle
-            buttonText
-          }
-          html
+  query($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html {
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          slug
+          title
         }
       }
     }
