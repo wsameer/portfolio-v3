b36cea5cf970e06ed114e7520a99c55468222f7a
nevrmore <dev.wsameer@gmail.com>
2020-07-22 19:37:27 +0530

    replaced hardcoded data with query result

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -97,48 +97,36 @@ const UnorderedList = styled.ul`
   }
 `;
 
-export const About = () => (
-  <Wrapper id="about">
-    <AboutWrapper as={Container}>
-      <PageTitle>About me</PageTitle>
-      <Row>
-        <Col xs={12} sm={4} md={4}>
-          <Thumbnail>
-            <img src={dev} alt="I’m Sameer and I’m a full-stack engineer!" />
-          </Thumbnail>
-        </Col>
-        <Col xs={12} sm={8} md={8}>
-          <Details>
-            <p>Hello! I am Sameer, a Senior Software Engineer based in Pune, India.</p>
-            <p>
-              I am a JavaScript ninja, offering 5+ years of experience in developing things that dwell on the internet.
-              Be it a static or a dynamic web application, custom component/plugin, responsive website, or anything in
-              between, I have got it all covered.
-            </p>
-            <p>
-              My aim is to deliver pixel perfect user interfaces built on top of modern JavaScript based frameworks
-              ensuring responsiveness and cross-browser support.
-            </p>
-            <p>Here are a few technologies I've been working with recently:</p>
-            <Row>
-              <Col xs="6">
-                <UnorderedList>
-                  <li>JavaScript (ES6+)</li>
-                  <li>React</li>
-                  <li>AngularJS & Angular</li>
-                </UnorderedList>
-              </Col>
-              <Col xs="6">
-                <UnorderedList>
-                  <li>HTML5 & (S)CSS</li>
-                  <li>Node.js</li>
-                  <li>ngrx/Redux</li>
-                </UnorderedList>
-              </Col>
-            </Row>
-          </Details>
-        </Col>
-      </Row>
-    </AboutWrapper>
-  </Wrapper>
-);
+export const About = ({ data }) => {
+  console.log(data);
+  const { frontmatter, html } = data[0].node;
+  const { pageTitle, skillsOne, skillsTwo, alt } = frontmatter;
+
+  return (
+    <Wrapper id="about">
+      <AboutWrapper as={Container}>
+        <PageTitle>{pageTitle}</PageTitle>
+        <Row>
+          <Col xs={12} sm={4} md={4}>
+            <Thumbnail>
+              <img src={dev} alt="I’m Sameer and I’m a full-stack engineer!" />
+            </Thumbnail>
+          </Col>
+          <Col xs={12} sm={8} md={8}>
+            <Details>
+              <div dangerouslySetInnerHTML={{ __html: html }} />
+              <Row>
+                <Col xs="6">
+                  <UnorderedList>{skillsOne && skillsOne.map((skill, i) => <li key={i}>{skill}</li>)}</UnorderedList>
+                </Col>
+                <Col xs="6">
+                  <UnorderedList>{skillsTwo && skillsTwo.map((skill, i) => <li key={i}>{skill}</li>)}</UnorderedList>
+                </Col>
+              </Row>
+            </Details>
+          </Col>
+        </Row>
+      </AboutWrapper>
+    </Wrapper>
+  );
+};
