46049195886aba6167915eb3f7a7369b31c63906
nevrmore <dev.wsameer@gmail.com>
2020-07-28 18:12:05 +0530

    removed contact form and added placeholder text

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import contact from 'assets/illustrations/contact.svg';
 import styled from 'styled-components';
-import { Container, Row, Col } from 'styles';
-import { ContactForm } from './ContactForm';
+import { Container, Row, Col, Button } from 'styles';
 
-const ContactWrapper = styled.div`
+const StyledContactWrapper = styled.div`
   padding: 4rem 0;
   display: block;
   justify-content: space-between;
@@ -14,62 +12,45 @@ const ContactWrapper = styled.div`
   }
 `;
 
-const PageTitle = styled.h2`
+const StyledPageTitle = styled.h2`
   font-size: 1.8rem;
   font-weight: 500;
   line-height: 1.2;
   margin-bottom: 3rem;
 `;
 
-const Details = styled.div`
-  padding-right: 2rem;
-
-  @media (max-width: 960px) {
-    padding-right: unset;
-    width: 100%;
-    order: 1;
-  }
-
-  h1 {
-    margin-bottom: 2rem;
-    font-size: 26pt;
-    color: #212121;
-  }
-
-  p {
-    margin-bottom: 2.5rem;
-    font-size: 20pt;
-    font-weight: normal;
-    line-height: 1.3;
-    color: #707070;
-  }
+const StyledMessage = styled.p`
+  font-size: 1.15rem;
+  font-weight: 400;
+  text-align: justify;
+  line-height: 30px;
+  color: #6c757d;
+  margin: 3rem 0;
 `;
 
-const Thumbnail = styled.div`
-  @media (max-width: 960px) {
-    width: 100%;
-    margin-bottom: 2rem;
-  }
+const StyledMailTo = styled(Button)`
+  background-color: transparent;
+  color: #f4eeeb;
+  border: 1px solid #fe5029;
 
-  img {
-    width: 100%;
+  :hover {
+    background-color: #fe5029;
+    color: #f4eeeb;
+    border-color: #fe5029;
   }
 `;
 
 export const Contact = () => (
-  <ContactWrapper as={Container} id="contact">
-    <PageTitle>Let's Build Something Together</PageTitle>
+  <StyledContactWrapper as={Container} id="contact">
+    <StyledPageTitle>Let's Build Something Together</StyledPageTitle>
     <Row>
       <Col sm={6}>
-        <Details>
-          <ContactForm />
-        </Details>
-      </Col>
-      <Col sm={6}>
-        <Thumbnail>
-          <img src={contact} alt="I’m Sameer and I’m a Full stack engineer!" />
-        </Thumbnail>
+        <StyledMessage>
+          Feel free to reach out if you're looking for a developer, have a question, want consultation on your project,
+          grab a coffee, or just want to say hi. I'll try my best to get back to you!
+        </StyledMessage>
+        <StyledMailTo href="mailto:dev.wsameer@gmail.com?Subject=Hello">dev.wsameer@gmail[dot]com</StyledMailTo>
       </Col>
     </Row>
-  </ContactWrapper>
+  </StyledContactWrapper>
 );
