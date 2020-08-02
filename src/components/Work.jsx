8991735bf7e1a3e999ea1e881bf064ed3426615a
nevrmore <dev.wsameer@gmail.com>
2020-08-02 11:44:58 +0530

    Added github graphql api data

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -92,61 +92,63 @@ const Stats = styled.div`
   }
 `;
 
-export const Work = () => (
-  // const {
-  //   github: {
-  //     viewer: {
-  //       repositories: { edges },
-  //     },
-  //   },
-  // } = useStaticQuery(
-  //   graphql`
-  //     {
-  //       github {
-  //         viewer {
-  //           repositories(first: 8, orderBy: { field: STARGAZERS, direction: DESC }) {
-  //             edges {
-  //               node {
-  //                 id
-  //                 name
-  //                 url
-  //                 description
-  //                 stargazers {
-  //                   totalCount
-  //                 }
-  //                 forkCount
-  //               }
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   `
-  // );
+export const Work = () => {
+  const {
+    github: {
+      viewer: {
+        repositories: { edges },
+      },
+    },
+  } = useStaticQuery(
+    graphql`
+      {
+        github {
+          viewer {
+            repositories(first: 10, orderBy: { field: STARGAZERS, direction: DESC }) {
+              edges {
+                node {
+                  id
+                  name
+                  url
+                  description
+                  stargazers {
+                    totalCount
+                  }
+                  forkCount
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  );
 
-  <WorksSection id="work">
-    <WorksWrapper as={Container}>
-      <PageTitle>Work</PageTitle>
-      {/* {edges.map(({ node }) => (
-          <Item key={node.id} as="a" href={node.url} target="_blank" rel="noopener noreferrer">
-            <Card>
-              <Content>
-                <h4>{node.name}</h4>
-                <p>{node.description}</p>
-              </Content>
-              <Stats>
-                <div>
-                  <img src={starIcon} alt="stars" />
-                  <span>{node.stargazers.totalCount}</span>
-                </div>
-                <div>
-                  <img src={forkIcon} alt="forks" />
-                  <span>{node.forkCount}</span>
-                </div>
-              </Stats>
-            </Card>
-          </Item>
-        ))} */}
-    </WorksWrapper>
-  </WorksSection>
-);
+  return (
+    <WorksSection id="work">
+      <WorksWrapper as={Container}>
+        <PageTitle>Work</PageTitle>
+        <Grid>
+          {edges.map(({ node }) => (
+            <Item key={node.id} as="a" href={node.url} target="_blank" rel="noopener noreferrer">
+              <Card>
+                <Content>
+                  <h4>{node.name}</h4>
+                  <p>{node.description}</p>
+                </Content>
+                <Stats>
+                  <div>
+                    <span>{node.stargazers.totalCount}</span>
+                  </div>
+                  <div>
+                    <span>{node.forkCount}</span>
+                  </div>
+                </Stats>
+              </Card>
+            </Item>
+          ))}
+        </Grid>
+      </WorksWrapper>
+    </WorksSection>
+  );
+};
